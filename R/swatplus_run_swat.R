3a44b9efd5d4eb47d34841519ae45aad27e8c093
Mike-Fuchs <m.d.fuchs@live.de>
2024-05-21 13:24:29 +0200

    add sleep

--- R/swatplus_run_swat.R
+++ R/swatplus_run_swat.R
@@ -436,6 +436,7 @@ run_swatplus <- function(project_path, output, parameter = NULL,
 
     ## Execute the SWAT exe file located in the thread folder
     for(lll in 1:n_tries){
+		Sys.sleep(runif(1,0,10))
 		msg <- run(run_os(swat_exe, os), wd = thread_path, error_on_status = FALSE)
 		if(any(grepl("Execution successfully completed",readLines("simulation.out")))){
 		break
